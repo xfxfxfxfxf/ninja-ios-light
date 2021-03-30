5674f11b64a03b7ebb20f9ada49dbabf0e8d4642
hopwesley <ws@sinoc.io>
2021-03-30 21:42:20 +0800

    init

---
+++ ninja/ninja-light/contact/ContactViewControllerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ninja
+//
+//  Created by wesley on 2021/3/30.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+
+        override func viewDidLoad() {
+                super.viewDidLoad()
+                // Do any additional setup after loading the view.
+        }
+
+
+}
+
---
+++ ninja/ninja-light/message/MsgViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MsgViewController.swift
+//  ninja
+//
+//  Created by wesley on 2021/3/30.
+//
+
+import UIKit
+
+class MsgViewController: UIViewController {
+        @IBOutlet weak var sender: UITextView!
+        @IBOutlet weak var receiver: UITextView!
+        
+        override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
